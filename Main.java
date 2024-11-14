a545993c55cb12ec4850253b0265c65160b4eb5d
Suraj <108755893+surajgajare123@users.noreply.github.com>
2024-11-14 17:22:10 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String[] args){
+        System.out.println("Performing break codition");
+        for(int i = 0; i <= 23; i++){
+            System.out.println("printing numbers!");
+            if(i == 6){
+                System.out.println("Ending loop here!");
+                break;
+            }
+        }
+    }
+}
